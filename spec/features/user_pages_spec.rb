606761e9cb07e170188696d26a70c1baf1e5633c
healthypackrat <healthypackrat@gmail.com>
2018-09-17 01:14:47 +0900

    Add user profile page

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -3,6 +3,17 @@ require 'rails_helper'
 RSpec.feature "User pages", type: :feature do
   subject { page }
 
+  describe "profile page" do
+    let(:user) { create(:user) }
+
+    before do
+      visit user_path(user)
+    end
+
+    it { should have_text(user.name) }
+    it { should have_title(user.name) }
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
