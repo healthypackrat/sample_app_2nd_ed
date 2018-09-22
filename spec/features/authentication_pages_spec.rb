2dabe616f1b8f3eb95f15b1d684af14169bb3642
healthypackrat <healthypackrat@gmail.com>
2018-09-22 20:00:54 +0900

    Test links for non-signed-in user

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -14,10 +14,18 @@ RSpec.feature "Authentication", type: [:feature, :request] do
     before { visit signin_path }
 
     describe "with invalid information" do
+      let(:user) { create(:user) }
+
       before { click_button 'Sign in' }
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-danger', text: 'Invalid') }
+
+      it { should_not have_link('Users', href: users_path) }
+      it { should_not have_link('Profile', href: user_path(user)) }
+      it { should_not have_link('Settings', href: edit_user_path(user)) }
+      it { should_not have_link('Sign out', href: signout_path) }
+      it { should have_link('Sign in', href: signin_path) }
 
       describe "after visiting another page" do
         before { click_link 'Home' }
