8bff6aed6469bd0d13c727ffb8b20b522be72f7c
healthypackrat <healthypackrat@gmail.com>
2018-09-28 00:21:55 +0900

    Implement follow/unfollow button

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -69,6 +69,51 @@ RSpec.feature "User pages", type: [:feature, :request] do
       it { should have_text(m2.content) }
       it { should have_text(user.microposts.count) }
     end
+
+    describe "follow/unfollow buttons" do
+      let(:other_user) { create(:user) }
+
+      before { sign_in user }
+
+      describe "following a user" do
+        before { visit user_path(other_user) }
+
+        it "should increment the followed user count" do
+          expect { click_button 'Follow' }.to change(user.followed_users, :count).by(1)
+        end
+
+        it "should increment the other user's followers count" do
+          expect { click_button 'Follow' }.to change(other_user.followers, :count).by(1)
+        end
+
+        describe "toggling the button" do
+          before { click_button 'Follow' }
+
+          it { should have_xpath('//input[@value="Unfollow"]') }
+        end
+      end
+
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
+
+        it "should decrement the followed user count" do
+          expect { click_button 'Unfollow' }.to change(user.followed_users, :count).by(-1)
+        end
+
+        it "should decrement the other user's followers count" do
+          expect { click_button 'Unfollow' }.to change(other_user.followers, :count).by(-1)
+        end
+
+        describe "toggling the button" do
+          before { click_button 'Unfollow' }
+
+          it { should have_xpath('//input[@value="Follow"]') }
+        end
+      end
+    end
   end
 
   describe "signup page" do
